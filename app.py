ce3efd98b7161f14095611d1062386c5eb17b3a7
Bartolomé Sánchez Salado <bartolome.salado@kiwi.com>
2023-01-02 21:05:41 +0100

    Add example app

---
+++ app.py
@@ -0,0 +1,7 @@
+import connexion
+from swagger_ui_bundle import swagger_ui_4_path
+
+options = {"swagger_path": swagger_ui_4_path}
+app = connexion.FlaskApp(__name__, specification_dir='openapi/', options=options)
+app.add_api('my_api.yaml')
+app.run(port=8000)
